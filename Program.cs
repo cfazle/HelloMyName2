809e3212f238d0c5e8f115cfbfc9d0dd96c8113a
Fazle <cfazle@gmail.com>
2020-05-30 22:32:36 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloMyName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string fname = "Fazle";
+            string lname = "Choudhury";
+            string icecream = "Cookie Dough";
+            Console.WriteLine("My name is: {0} {1}", fname, lname);
+            Console.WriteLine("I like icecream: {0}", icecream);
+
+        }
+    }
+}
